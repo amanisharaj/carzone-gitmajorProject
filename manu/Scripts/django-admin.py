99d750103275509f53d91e8ce6e25efe1fa81d54
Manisha Raj <amanisharaj@gmail.com>
2021-04-23 16:06:39 +0530

    my frist commit

---
+++ manu/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\manisha raj\desktop\4-2_sem\major_project\carzone\manu\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
